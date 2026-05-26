418c0d4c0eed347aa79630bc17c48eb7d1ee0f8b
pietrowzy <pietrozeffirele@gmail.com>
2026-05-25 21:38:00 -0300

    fix(docs): Ajustes de comentários no código do arquivo db.py

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,23 @@
+# ==========================================================
+# IMPORTAÇÕES
+# ==========================================================
+
+# Recursos usados para conectar o sistema ao banco de dados MySQL.
 import mysql.connector
 from mysql.connector import Error
 from config import DB_CONFIG
 
+# ==========================================================
+# CONEXÃO COM O BANCO DE DADOS
+# ==========================================================
+
 def get_connection():
+
+    # Cria e retorna uma conexão com o MySQL usando as configurações do projeto.
     try:
         connection = mysql.connector.connect(**DB_CONFIG)
         return connection
+
     except Error as e:
         print("Erro ao conectar ao MySQL:", e)
         return None
